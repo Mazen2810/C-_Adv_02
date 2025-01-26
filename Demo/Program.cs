7866209e551cec478b0a02e371e86b9e9a307ead
Mazen2810 <mazenmuhammed52@gmail.com>
2025-01-26 17:21:23 +0200

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList Numbers = new ArrayList();
+        }
+    }
+}
