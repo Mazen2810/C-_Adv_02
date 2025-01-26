a94077835149025be5ce108df01a41af79422e95
Mazen2810 <mazenmuhammed52@gmail.com>
2025-01-26 17:28:03 +0200

    Non-Generic Collections - ArrayList

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -3,9 +3,34 @@ namespace Demo
 {
     internal class Program
     {
+        public static int SumArrayList(ArrayList Numbers)
+        {
+            int Sum = 0;
+
+            if (Numbers is not null)
+            {
+                for(int i = 0; i < Numbers.Count; i++)
+                {
+                    Sum += (int?) Numbers[i] ?? 0; // Casting from Object [Reference Type] to int [Value Type]
+                                                   // UnBoxing [Unsafe Casting]
+                }
+            }
+            return Sum;
+        }
         static void Main(string[] args)
         {
-            ArrayList Numbers = new ArrayList();
+
+            #region Non-Generic Collections - ArrayList
+            //ArrayList Numbers = new ArrayList(5);
+            //Numbers.Add(1); // Casting from int [Value type] to object [Reference type] --> Boxing
+            //Numbers.Add(2); // Casting from int [Value type] to object [Reference type] --> Boxing
+            //Numbers.Add(3); // Casting from int [Value type] to object [Reference type] --> Boxing
+            //Numbers.Add(4); // Casting from int [Value type] to object [Reference type] --> Boxing
+            //Numbers.Add(5); // Casting from int [Value type] to object [Reference type] --> Boxing
+
+            //int Result = SumArrayList(Numbers);
+            //Console.WriteLine($"Result = {Result}"); 
+            #endregion
         }
     }
 }
